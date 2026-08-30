bb3ec08f539976d20533f8beab2f8b6f4129e9c8
240713036 <s240713036@g.tohoku-gakuin.ac.jp>
2026-08-30 17:18:01 +0900

    Add files via apload

---
+++ news.js
@@ -0,0 +1,72 @@
+/*新しい情報は上に追加します。以下でひとかたまりです
+
+  {
+    title: "タイトル",
+    date: "yyyy-mm-dd",
+    link: "リンクは任意"
+  },
+
+  {
+    title: "タイトル",
+    date: "yyyy-mm-dd",
+  },
+
+*/
+
+
+const newsList = [
+  { 
+    title: "ホームページを公開しました", 
+    date: "2026-08-31"
+  },
+  {
+    title: "2026年度第5回木曜アカデミックカフェのご案内 - 東北学院大学",
+    date: "2026-07-25",
+    link: "https://www.tohoku-gakuin.ac.jp/info/top/260714-4.html"
+  },
+  {
+    title: "2026年度第4回木曜アカデミックカフェのご案内 - 新着情報｜東北学院大学",
+    date: "2026-07-13",
+    link: "https://www.tohoku-gakuin.ac.jp/info/top/260710-4.html"
+  },
+  {
+    title: "2026年度第3回木曜アカデミックカフェのご案内（7/9） - 新着情報｜東北学院大学",
+    date: "2026-06-26",
+    link: "https://www.tohoku-gakuin.ac.jp/info/top/260625-3.html"
+  },
+  {
+    title: "2026年度第2回木曜アカデミックカフェのご案内（6/18） - 新着情報｜東北学院大学",
+    date: "2026-06-06",
+    link: "https://www.tohoku-gakuin.ac.jp/info/top/260604-1.html"
+  },
+  {
+    title: "2026年度第1回木曜アカデミックカフェのご案内（4/30） - 新着情報｜東北学院大学",
+    date: "2026-04-20",
+    link: "https://www.tohoku-gakuin.ac.jp/info/top/260416-2.html"
+  },
+  {
+    title: "第9回木曜アカデミックカフェのご案内（3/19） - 新着情報｜東北学院大学",
+    date: "2026-03-10",
+    link: "https://www.tohoku-gakuin.ac.jp/info/top/260309-2.html"
+  },
+  {
+    title: "第8回「木曜アカデミックカフェのご案内」（2/26） - 新着情報｜東北学院大学",
+    date: "2026-02-18",
+    link: "https://www.tohoku-gakuin.ac.jp/info/top/260216-1.html"
+  },
+  {
+    title: "第7回「木曜アカデミックカフェのご案内」 - 新着情報｜東北学院大学", 
+    date: "2026-01-17", 
+    link: "https://www.tohoku-gakuin.ac.jp/info/top/260115-1.html"
+  },
+  {
+    title: "第3回 情報学シンポジウム開催のお知らせ(12/20) - 東北学院大学", 
+    date: "2025-11-09", 
+    link: "https://www.tohoku-gakuin.ac.jp/info/top/251107-1.html"
+  },
+  {
+    title: "第5回「木曜アカデミックカフェのご案内」（11/13） - 新着情報｜東北学院大学",
+    date: "2025-11-07",
+    link: "https://www.tohoku-gakuin.ac.jp/info/top/251106-1.html"
+  }
+];
